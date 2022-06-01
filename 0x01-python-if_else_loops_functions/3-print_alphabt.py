479f2da7b60bc7aefe0761ec4d2fb6bf2817afa4
holydexboi <102956651+zulikifPunch@users.noreply.github.com>
2022-06-01 03:30:51 +0100

    print alhb

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
+    if chr(letter) == 'q' and chr(letter) == 'e':
+        continue
+    else:
         print("{}".format(chr(letter)), end="")
